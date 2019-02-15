313714474539ea8d6bfb874ae05391d65625a842
pushyami <pushyami@umich.edu>
2019-02-15 18:07:01 -0500

    adding env to the list

--- cron_test.py
+++ cron_test.py
@@ -1,7 +1,10 @@
 from datetime import datetime
+import os
 
 def start():
     print(f"starting now:{datetime.now()}")
+    print(f"environmental variable: {os.getenv('course')}")
+
 
 if __name__ == '__main__':
     start()

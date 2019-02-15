0064be3c58820d2d6f2fb39f050995b3513bbdeb
pushyami <pushyami@umich.edu>
2019-02-15 18:35:41 -0500

    persisted layer test

--- cron_test.py
+++ cron_test.py
@@ -7,16 +7,13 @@ def start():
     try:
         with open('/opt/dev/persisted.txt') as f:
             latest_exam_date: str = f.read()
-            print(f"""reading from to file persisted.txt the latest test taken date in
-                                UTC with current timestamp""")
-            return latest_exam_date.strip()
+            print(latest_exam_date.strip())
     except (OSError, IOError, Exception) as e:
         print(f"failed do read the file persisted.txt due to {e}")
 
     try:
         with open('/opt/dev/persisted.txt', 'w+') as f:
-            print(f"""writing to file persisted.txt the next query date/latest test taken date in
-                                UTC """)
+            print(f"writing to file persisted.txt" )
             f.write('success writing to the file')
     except (OSError, IOError, Exception) as e:
         print(f"""failed do write file failed due to {e}""")

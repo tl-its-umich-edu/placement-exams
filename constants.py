a4b7eaab1d76b3821a800bf6c5964a44750f870f
Sam Sciolla <ssciolla@umich.edu>
2020-06-22 11:19:44 -0400

    Simplify constants, move to root level; delete spe_utils

---
+++ constants.py
@@ -0,0 +1,12 @@
+ISO8601_FORMAT = '%Y-%m-%dT%H:%M:%SZ'
+
+CANVAS_SCOPE = 'canvasreadonly'
+MPATHWAYS_SCOPE = 'spanishplacementscores'
+
+SCORE_RANDOMIZER_FOR_TEST = 'test_enable_score_randomizer'
+TEST = 'test'
+SMPT_DEBUG = 'smpt_debug'
+SMPT_FROM = 'smpt_from'
+SMPT_TO = 'smpt_to'
+SMPT_HOST = 'smpt_host'
+SMPT_PORT = 'smpt_port'

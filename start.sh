88f5fc3c5cb0a248d8d25a6fdb1231f945065cc7
Sam Sciolla <ssciolla@umich.edu>
2020-06-25 18:05:49 -0400

    Remove SPE test suite (will refer to it for future report tests)

--- start.sh
+++ start.sh
@@ -18,7 +18,6 @@ python manage.py loaddata "$FIXTURES_FILE"
 
 if [ "${TEST_MODE}" == "True" ]; then
     echo "Running tests"
-    # python -m unittest test/spe_test.py
     python manage.py test -v 3
 elif [ -z "${CRONTAB_SCHEDULE}" ]; then
     echo "CRONTAB_SCHEDULE environment variable was not set; a one-time job will be triggered"
